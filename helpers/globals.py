42675ac4d046b94c13da485dba2d7f3105075e81
rubendgpedrosa <rubendgpedrosa@pop-os.localdomain>
2022-06-18 20:58:08 +0100

    Shuffled db stuff to env file

--- helpers/globals.py
+++ helpers/globals.py
@@ -50,3 +50,8 @@ def init():
     
     global DOCKER_CLIENT
     DOCKER_CLIENT = docker.from_env().api
+
+    global DB_NAME, DB_USER, DB_PASSWORD
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
