c869b6e733d17132087060d2764d5f2b6d617d29
rubendgpedrosa <ruben@riseup.ai>
2022-07-29 18:12:30 +0100

    remove logs

--- helpers/scanner_manager.py
+++ helpers/scanner_manager.py
@@ -1,7 +1,7 @@
 import os, json, requests, datetime
 
 import helpers.globals as globals
-from helpers.utilities import build_query, build_embed_object_title_description, log_actions
+from helpers.utilities import build_query, build_embed_object_title_description
 from helpers.notifications import fetch_new_pvp_data
 from helpers.scanner_status import check_map_status
 from helpers.data_quests_handler import fetch_today_data, verify_quest_scan_done
@@ -14,23 +14,17 @@ def start_pokestop_scan():
     set_quest_scanning_state(1)
     fetch_new_pvp_data()
     fetch_today_data()
-    log_actions("Resetting alarm container")
     globals.DOCKER_CLIENT.stop(globals.ALARM_CONTAINER)
     globals.DOCKER_CLIENT.wait(globals.ALARM_CONTAINER)
     globals.DOCKER_CLIENT.start(globals.ALARM_CONTAINER)
-    log_actions("Exiting script")
     exit()
 
 async def is_quest_scanning():
-    log_actions("Checking if quest scanning is enabled...")
     execId = globals.DOCKER_CLIENT.exec_create(globals.DB_CONTAINER, build_query("SELECT scanned FROM poliswag WHERE scanned = 1;", "poliswag"))
     questResults = globals.DOCKER_CLIENT.exec_start(execId)
-    log_actions("Quest is scanning: " + str(len(str(questResults).split("\\n")) > 1))
     if len(str(questResults).split("\\n")) > 1:
         fetch_today_data()
-        log_actions("Updating today's quest data...")
         if verify_quest_scan_done():
-            log_actions("Quest scan has been completed")
             set_quest_scanning_state()
             channel = globals.CLIENT.get_channel(globals.QUEST_CHANNEL_ID)
             await channel.send(embed=build_embed_object_title_description(
@@ -41,7 +35,6 @@ async def is_quest_scanning():
             )
     else:
         if datetime.datetime.now().day > globals.CURRENT_DAY:
-            log_actions("Initializing quest scan...")
             globals.CURRENT_DAY = datetime.datetime.now().day
             start_pokestop_scan()
         await check_map_status()
@@ -49,7 +42,6 @@ async def is_quest_scanning():
 def set_quest_scanning_state(disabled = 0):
     execId = globals.DOCKER_CLIENT.exec_create(globals.DB_CONTAINER, build_query(f"UPDATE poliswag SET scanned = {disabled};", "poliswag"))
     globals.DOCKER_CLIENT.exec_start(execId)
-    log_actions("Changed state to: " + str(disabled))
 
 def clear_old_pokestops_gyms():
     execId = globals.DOCKER_CLIENT.exec_create(globals.DB_CONTAINER, 
