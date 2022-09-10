ca65b851fd0b9b49b3fc35f9f95ac94c5164548c
rubendgpedrosa <ruben@riseup.ai>
2022-09-10 23:43:42 +0100

    [P44] checking for box state through python instead of bash for better maintainability

--- helpers/scanner_status.py
+++ helpers/scanner_status.py
@@ -11,27 +11,31 @@ boxUsersData = [
 ]
 
 async def check_boxes_issues():
-    # execId = globals.DOCKER_CLIENT.exec_create(globals.DB_CONTAINER, build_query("SELECT settings_device.name FROM trs_status LEFT JOIN settings_device ON trs_status.device_id = settings_device.device_id WHERE trs_status.device_id < 14 AND TIMESTAMPDIFF(SECOND, trs_status.lastProtoDateTime, NOW()) > 900;"))
-    # boxStatusResults = globals.DOCKER_CLIENT.exec_start(execId)
-    # listBoxStatusResults = str(boxStatusResults).split("\\n").remove("b''")
-    listBoxStatusResults = ["Tx9s1", "a95xF1"]
+    # 4500 since it's 900 seconds + 1 hour (vps timezone differences)
+    execId = globals.DOCKER_CLIENT.exec_create(globals.DB_CONTAINER, build_query("SELECT settings_device.name FROM trs_status LEFT JOIN settings_device ON trs_status.device_id = settings_device.device_id WHERE trs_status.device_id < 14 AND TIMESTAMPDIFF(SECOND, trs_status.lastProtoDateTime, NOW()) > 4500;"))
+    boxStatusResults = globals.DOCKER_CLIENT.exec_start(execId)
+    listBoxStatusResults = str(boxStatusResults).split("\\n").remove("b''")
     if len(listBoxStatusResults) > 1:
         for box in listBoxStatusResults:
-            # We replace this value since it's different in the db
+            # Edge case where we replace this value since it's different in the db
             if box == "PoGoLeiria":
                 box = "Tx9s3_JMBoy"
             for boxuser in boxUsersData:
-                if box in boxuser["boxes"]:
-                    user = await globals.CLIENT.fetch_user(boxuser["mention"])
-                    message = await user.send(embed=build_embed_object_title_description("Box " + box + " está com problemas."))
+                elo = 1
+                #if box in boxuser["boxes"]:
+                    #user = globals.CLIENT.fetch_user(boxuser["mention"])
+                    #message = user.send(embed=build_embed_object_title_description("Box " + box + " está com problemas."))
         await rename_voice_channel(len(listBoxStatusResults))
 
 #await rename_voice_channel(message.content)
 async def rename_voice_channel(totalBoxesFailing):
-    message = ""
-    if totalBoxesFailing == 0:
-        message = "Boxes com problemas: " + str(totalBoxesFailing)
-    await globals.CLIENT.get_channel(globals.VOICE_CHANNEL_ID).edit(name=f"{totalBoxesFailing}")
+    message = "SCANNER: 🟢"
+    if totalBoxesFailing > 0 and totalBoxesFailing < 7:
+        message = "SCANNER: 🟡"
+    if totalBoxesFailing == 7:
+        message = "SCANNER: 🔴"
+    voiceChannel = globals.CLIENT.get_channel(globals.VOICE_CHANNEL_ID)
+    await voiceChannel.edit(name=message)
 
 async def check_map_status():
 #70mins since the mysql timezone and vps timezone have an hour differente. 60mins + 10mins
